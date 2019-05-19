c561a67ce08a3a31d282ba55009494d6d4b117fb
MatsushimaMoe <>
2019-05-19 21:11:04 +0800

    responsive of slider (app:js/index.html) addition of fonts"SawarabiGothic"(style.css/index.html)

--- self-web/app.js
+++ self-web/app.js
@@ -1,7 +1,24 @@
 //console.log('あ');
 $('.js-stamp').slick({
+  dots:true,
   infinite: true,
   slidesToShow: 4,
-  slidesToScroll: 4
+  slidesToScroll: 4,
+  
+  responsive:[
+    {
+      breakpoint: 960,
+        settings:{
+          slidesToShow:3,
+          slidesToScroll:3
+        }
+    },{
+      breakpoint: 480,
+        settings:{
+          slidesToShow:2,
+          slidesToScroll:2
+        }
+    }
+  ]
   
 });

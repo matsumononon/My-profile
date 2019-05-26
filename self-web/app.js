9839fa5d036f32e320fe3087cd504cdaf83b729b
MatsushimaMoe <>
2019-05-26 22:37:25 +0800

    add sass code

--- self-web/app.js
+++ self-web/app.js
@@ -4,6 +4,7 @@ $('.js-stamp').slick({
   infinite: true,
   slidesToShow: 4,
   slidesToScroll: 4,
+  variableWidth:true,
   
   responsive:[
     {
@@ -17,8 +18,28 @@ $('.js-stamp').slick({
         settings:{
           slidesToShow:2,
           slidesToScroll:2
+          
         }
     }
   ]
   
 });
+//$(function() {
+//    var topBtn = $('#page-top');    
+//   //スクロールしてトップ
+//    topBtn.click(function () {
+//        $('body,html').animate({
+//            scrollTop: 0
+//        }, 500);
+//        return false;
+//    });
+//});
+
+//トップに戻る=スムーズクロール
+  $('.js-scroll').on('click',function(){
+    $('body,html').animate({scrollTop:0},1000);
+  });
+//  
+//  $('.js-scroll').on('click',function(){
+//    $('スクロール対象=body,html').animate({sdrollTop:座標},時間);
+//  });
